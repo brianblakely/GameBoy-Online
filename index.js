b1bc9ed6b88508ea4390bc1d0e0fa10f0c04c4a1
Brian Blakely <anewpage.media@gmail.com>
2020-02-23 10:48:37 -0500

    Fix core export.

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
 import loader from "@assemblyscript/loader";
 
-(async function main() {
-  const imports = {
-    console: {
-      debug: msgPtr => {
-        process.env.NODE_ENV === `development` &&
-          console.debug(core.getString(msgPtr));
-      },
-      info: msgPtr => {
-        process.env.NODE_ENV === `development` &&
-          console.info(core.getString(msgPtr));
-      },
-      warn: msgPtr => {
-        process.env.NODE_ENV === `development` &&
-          console.warn(core.getString(msgPtr));
-      },
-      error: msgPtr => {
-        process.env.NODE_ENV === `development` &&
-          console.error(core.getString(msgPtr));
-      }
+const imports = {
+  console: {
+    debug: msgPtr => {
+      process.env.NODE_ENV === `development` &&
+        console.debug(core.getString(msgPtr));
+    },
+    info: msgPtr => {
+      process.env.NODE_ENV === `development` &&
+        console.info(core.getString(msgPtr));
+    },
+    warn: msgPtr => {
+      process.env.NODE_ENV === `development` &&
+        console.warn(core.getString(msgPtr));
+    },
+    error: msgPtr => {
+      process.env.NODE_ENV === `development` &&
+        console.error(core.getString(msgPtr));
     }
-  };
+  }
+};
 
-  /*`loader.instantiate` will use `WebAssembly.instantiateStreaming`
+/*`loader.instantiate` will use `WebAssembly.instantiateStreaming`
    if possible. Overwise fallback to `WebAssembly.instantiate`. */
-  const core = await loader.instantiate(fetch(`optimized.wasm`), imports);
-})();
+let core;
+loader
+  .instantiate(fetch(`optimized.wasm`), imports)
+  .then(module => (core = module));
+
+export default core;
